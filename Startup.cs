78a58c2e0e7f904422083b7990c367714bfeab16
andol <andol@KEVIN-PC>
2022-03-09 15:10:23 +0300

    Ajoutez des fichiers projet.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FluxManager.Startup))]
+namespace FluxManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
